0266e0184da8e12043ecb2d804b9cc02fbea1c34
Thomas Gusewelle <101810704+gkwmedia@users.noreply.github.com>
2023-05-18 13:32:57 -0500

    create and delete mappings

--- src/main/java/com/thomasgusewelle/it634/airtravel/restControllers/airportRestController.java
+++ src/main/java/com/thomasgusewelle/it634/airtravel/restControllers/airportRestController.java
@@ -4,8 +4,6 @@ import com.thomasgusewelle.it634.airtravel.models.Airport;
 import com.thomasgusewelle.it634.airtravel.repositories.airportRepository;
 import com.thomasgusewelle.it634.airtravel.models.wrappers.AirportListWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,29 +11,45 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+// Denotes that the class is a REST controller and puts the base path at api/airport
 @RestController
 @RequestMapping("api/airport")
 public class airportRestController {
+//    Sets up the repo for the airport table
     @Autowired
     private airportRepository repo;
 
+//    mapping for getting all of the airports
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AirportListWrapper> getAllAirports() {
         AirportListWrapper wrapper = new AirportListWrapper();
         List<Airport> airports = repo.findAll();
         wrapper.setAirports(airports);
-        System.out.println("this is the airports " + airports);
         return ResponseEntity.ok().body(wrapper);
     }
 
+//    Post for creating an airport
+    @PostMapping("/create")
+    public Airport createAirport(@RequestBody Airport airport){
+       return repo.save(airport);
+    }
+
+//    Returns the airport based on an id if it exists
     @GetMapping("/{id}")
     public ResponseEntity<Airport> getAirportById(@PathVariable(value = "id") String id) {
         Optional<Airport> airport = repo.findById(id);
         return airport.map(value -> ResponseEntity.ok().body(value)).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+//    Post for updating an airport
     @PostMapping("/{id}")
     public Airport updateAirportById(@PathVariable(value = "id") String id, @RequestBody Airport airport){
         return repo.save(airport);
+    }
+//    Delete for deleting an airport. First finds the airport and if it exists deletes it.
+    @DeleteMapping("/{id}")
+    public void deleteAirport(@PathVariable(value = "id") String id){
+        Optional<Airport> airport = repo.findById(id);
+        airport.ifPresent(value -> repo.delete(value));
     }
 }
